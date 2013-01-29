fcabfb8265e546f447b6ce16d900d2348cb5330f
= <Neugebauer_David@web.de>
2013-01-29 14:13:06 +0100

    Added basic scaffolding for a reading loop.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,53 @@
 
 using namespace std;
 
+#include <fstream>
+
+
+void read(ifstream& file);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    ifstream file;
+    file.open("test-data.txt");
+    read(file);
+
     return 0;
 }
 
+
+void read(ifstream& file)
+{
+    char symbol;
+    char buffer[80];
+    int counter;
+
+    /**
+      * Basis-Leseschleife
+      */
+
+    for (counter = 0, file.get(symbol); !file.eof(); file.get(symbol))
+    {
+        switch(symbol){
+        case ':':
+            buffer[counter] = '\0';
+            counter = 0;
+            cout << '\'' << buffer << '\'' << endl;
+            break;
+        case '.':
+            buffer[counter] = '\0';
+            counter = 0;
+            cout << buffer << endl;
+            break;
+        case '\n':
+            buffer[counter] = '\0';
+            counter = 0;
+            cout << buffer << endl;
+            break;
+        default:
+            buffer[counter] = symbol;
+            counter += 1;
+            break;
+        }
+    }
+}
