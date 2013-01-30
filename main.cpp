ac7743519ffe5ebf69c32853d6a41bfa2ea7af96
= <Neugebauer_David@web.de>
2013-01-30 18:11:47 +0100

    Added handling of line endings.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 #include <fstream>
 #include <string.h>
+#include <ios>
 
 
-void read(ifstream& datei);
+void read(ifstream& file);
+int isAtCategoryLineStart(ifstream &file);
 
 enum datasetStatus { inside, outside };
-enum categoryStatus { number, info, noise };
+enum categoryStatus { number, info, end };
 
 int main()
 {
     ifstream file;
-    file.open("test-data.txt");
+    file.open("test-data-2.txt");
+    if (!file) {
+        cout << "Keine Datei mit diesem Namen vorhanden." << endl;
+    }
     read(file);
     file.close();
 
@@ -22,16 +27,16 @@ int main()
 }
 
 
-void read(ifstream& datei)
+void read(ifstream& file)
 {
     char symbol;
     char buffer[80] = "\0";
     int counter;
 
     enum datasetStatus datasetStatus = outside;
-    enum categoryStatus categoryStatus = noise;
+    enum categoryStatus categoryStatus = end;
 
-    for (counter = 0, datei.get(symbol); !datei.eof(); datei.get(symbol))
+    for (counter = 0, file.get(symbol); !file.eof(); file.get(symbol))
     {
         switch(symbol){
 
@@ -39,6 +44,8 @@ void read(ifstream& datei)
 
             // Datensatzbeginn
             if (datasetStatus == outside) {
+                buffer[counter] = '\0';
+                counter = 0;
                 if (!strcmp(buffer, "000")) {
                     datasetStatus = inside;
                     cout << "------ Beginn Datensatz ------" << endl;
@@ -47,21 +54,22 @@ void read(ifstream& datei)
 
             // Kategorienummer behandeln, wenn im Datensatz
             if (datasetStatus == inside) {
+
                 buffer[counter] = '\0';
                 counter = 0;
 
                 // Datensatzende behandeln
                 if (!strcmp(buffer, "999")) {
                     datasetStatus = outside;
-                    categoryStatus = noise;
+                    categoryStatus = end;
                     cout << "------ Ende Datensatz ------" << endl;
                 }
                 // Normale Kategorienummer behandeln
                 else {
+                    categoryStatus = info;
                     cout << "KatNr.: " << buffer << endl;
                 }
             }
-
 
             break;
 
@@ -72,8 +80,27 @@ void read(ifstream& datei)
             break;
 
         case '\n':
-            buffer[counter] = '\0';
-            counter = 0;
+
+            // TODO:Auf fehlerhafte Eingaben zwischen Datansätzen prüfen
+            if (datasetStatus == outside) {
+                buffer[counter] = '\0';
+                counter = 0;
+                break;
+            }
+
+
+            if (categoryStatus == info) {
+                // Status der neuen Zeile prüfen
+                if (isAtCategoryLineStart(file)) {
+                    buffer[counter] = '\0';
+                    counter = 0;
+
+                    cout << "Inhalt: " << buffer << endl;
+
+                    categoryStatus = number;
+                }
+            }
+
 //            cout << buffer << endl;
             break;
 
@@ -84,4 +111,23 @@ void read(ifstream& datei)
 
         }
     }
+}
+
+int isAtCategoryLineStart(ifstream& file)
+{
+    int result = 1;
+    char c;
+    int i;
+    char word[3];
+    for (file.get(c), i = 0; i < 3; i++, file.get(c)) {
+        word[i] = c;
+        if (!std::isdigit(c)) {
+            result = 0;
+            break;
+        }
+    }
+    for (; i >= 0; i--) {
+        file.putback(word[i]);
+    }
+    return result;
 }
