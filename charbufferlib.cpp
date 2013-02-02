aac3eca24e81185a9d6997d03579522298b83427
= <Neugebauer_David@web.de>
2013-02-02 20:09:03 +0100

    Fixed a bug in behaviour of charbuffer when reallocating buffer space.

--- charbufferlib.cpp
+++ charbufferlib.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 ClCharBuffer::ClCharBuffer(int initSize)
 {
-    maxSize = initSize;
-    buffer = new char[initSize];
+    maxSize = (initSize < 2) ? 2 : initSize;
+    buffer = new char[maxSize];
     buffer[0] = '\0';
     counter = 0;
 }
@@ -18,7 +18,7 @@ ClCharBuffer::ClCharBuffer(int initSize)
 void ClCharBuffer::push(char c)
 {
     buffer[counter++] = c;
-    if (counter == maxSize) {
+    if (counter == maxSize-1) {
         maxSize *= 2;
         char *newBuffer = new char[maxSize];
         strcpy(newBuffer, getString());
